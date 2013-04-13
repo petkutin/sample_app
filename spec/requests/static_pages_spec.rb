525537f4e55b138aa0f8a530f5bb6eac9dcc80cc
petkutin <kutlay@tinbee.com>
2013-04-13 13:26:43 -0700

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home Page" do
+    it "should have the h1 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+     visit '/static_pages/home'
+     page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do 
+     visit '/static_pages/home'
+     page.should have_selector('title', :text => 'Tutorial | Home')
+    end
+  end
+
+  describe "Help page" do 
+    it "should have h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => 'Tutorial | Help')
+    end
+  end
+
+  describe "about page" do 
+    it "should have the h1 'About us'" do 
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    it "should have the title 'About us'" do 
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => 'Tutorial | About Us')
+    end
+  end
+end
+
